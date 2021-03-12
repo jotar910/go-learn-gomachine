01ace031a13ab77b1b58a7f522f2f1aaeb2356da
joao.paulo <joao.paulo@present-technologies.com>
2021-03-12 22:37:16 +0000

    first-step

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,12 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+type GoMachine struct {
+	Memory [DefaultMemSize]uint64
+	P uint64
+}
+
+func New() GoMachine {
+	return GoMachine{}
+}
